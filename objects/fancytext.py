82ee816f08782fa92146823f2c2740af3f163966
Mike Suhan <you@example.com>
2020-01-17 11:06:35 +0300

    Changed font settings

--- objects/fancytext.py
+++ objects/fancytext.py
@@ -3,7 +3,7 @@ import formatting as fm
 
 class FancyText(tk.Text):
 
-    def __init__(self, parent, width=90, background=fm.text_bg, font=fm.text_font, foreground=fm.text_fg, *args, **kwargs):
+    def __init__(self, parent, placeholder=None, width=90, background=fm.text_bg, font=fm.text_font, foreground=fm.text_fg, *args, **kwargs):
         tk.Text.__init__(self, parent, *args, **kwargs,
                          background=background,
                          font=font,
@@ -16,6 +16,14 @@ class FancyText(tk.Text):
         self.popup_menu.add_command(label="Select All", command=self.select_all)
 
         self.bind("<Button-3>", self.popup) # Button-2 on Aqua
+
+        if placeholder:
+            self.insert(tk.END, placeholder)
+            self.bind('<Button-1>', self.clear_placeholder)
+
+    def clear_placeholder(self, event):
+        self.delete(1.0, tk.END)
+        self.unbind('<Button-1>')
 
     def popup(self, event):
         try:
